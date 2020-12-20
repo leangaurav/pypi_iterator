d10584ce94af0b4a413c151e6cb7adc69e02af69
leangaurav <trainer.cpp@gmail.com>
2020-12-20 19:12:49 +0530

    setup action to publish release to pypi

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name="iterators",
     packages = ['iterators'],
-    version="0.0.1",
+    version="release_version",
     author="leangaurav",
     author_email="leangaurav.me@gmail.com",
     description="Iterator utility classes and functions",
