78d4e6ad24c5d47f6cd52b5c5614f4589f558c0c
leangaurav <leangaurav.me@gmail.com>
2022-10-25 00:55:34 +0530

    Add Async Timeout Iterator (#10)
    Co-authored-by: leangaurav <leanguarav.me@gmail.com>

--- iterators/timeout_iterator.py
+++ iterators/timeout_iterator.py
@@ -1,10 +1,11 @@
 import queue
+import asyncio
 import threading
 
 class TimeoutIterator:
     """
     Wrapper class to add timeout feature to synchronous iterators
-    - timeout: initial default timeout
+    - timeout: timeout for next(). Default=ZERO_TIMEOUT i.e. no timeout or blocking calls to next. Updated using set_timeout() 
     - sentinel: the object returned by iterator when timeout happens
     - reset_on_next: if set to True, timeout is reset to the value of ZERO_TIMEOUT on each iteration
 
@@ -90,3 +91,69 @@ class TimeoutIterator:
                     raise StopIteration()
         except BaseException as e:
             self._buffer.put(e)
+
+class AsyncTimeoutIterator:
+    """
+    Async version of TimeoutIterator. See method documentation of TimeoutIterator
+    """
+    ZERO_TIMEOUT = 0.0
+
+    def __init__(self, iterator, timeout=0.0, sentinel=object(), reset_on_next=False):
+        self._iterator = iterator
+        self._timeout = timeout
+        self._sentinel= sentinel
+        self._reset_on_next = reset_on_next
+
+        self._interrupt = False
+        self._done = False
+        self._buffer = asyncio.Queue()
+        self._task = asyncio.get_event_loop().create_task(self.__lookahead())
+
+    def get_sentinel(self):
+        return self._sentinel
+
+    def set_reset_on_next(self, reset_on_next):
+        self._reset_on_next = reset_on_next
+
+    def set_timeout(self, timeout: float):
+        self._timeout = timeout
+
+    def interrupt(self):
+        self._interrupt = True
+
+    def __iter__(self):
+        return self
+
+    async def __anext__(self):
+        if self._done:
+            raise StopAsyncIteration
+
+        data = self._sentinel
+        try:
+            if self._timeout > self.ZERO_TIMEOUT:
+                data = await asyncio.wait_for(self._buffer.get(), self._timeout)
+            else:
+                data = await self._buffer.get()
+        except asyncio.TimeoutError:
+            pass
+        finally:
+            # see if timeout needs to be reset
+            if self._reset_on_next:
+                self._timeout = self.ZERO_TIMEOUT
+
+        # propagate any exceptions including StopIteration
+        if isinstance(data, BaseException):
+            self._done = True
+            raise data
+
+        return data
+
+    async def __lookahead(self):
+        try:
+            while True:
+                data = await self._iterator.__anext__()
+                await self._buffer.put(data)
+                if self._interrupt:
+                    raise StopAsyncIteration()
+        except BaseException as e:
+            await self._buffer.put(e)
